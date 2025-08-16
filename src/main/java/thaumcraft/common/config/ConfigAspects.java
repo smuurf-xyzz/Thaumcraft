4d36f4ad141a4de40b41a3604a32184d3cc26fb9
S4TURN <s4turn21@gmail.com>
2025-08-16 15:26:28 +0300

    feat: add recipe hedge string

--- src/main/java/thaumcraft/common/config/ConfigAspects.java
+++ src/main/java/thaumcraft/common/config/ConfigAspects.java
@@ -52,11 +52,13 @@ public class ConfigAspects {
         ThaumcraftApi.registerObjectTag(new ItemStack(Items.CLAY_BALL), new AspectList().add(Aspect.WATER, 5).add(Aspect.EARTH, 5));
         ThaumcraftApi.registerObjectTag(new ItemStack(Blocks.TERRACOTTA), new AspectList().add(Aspect.WATER, 5).add(Aspect.EARTH, 5));
         ThaumcraftApi.registerObjectTag(BlockTags.LOGS, new AspectList().add(Aspect.PLANT, 20));
+        ThaumcraftApi.registerObjectTag(new ItemStack(Items.WHEAT), new AspectList().add(Aspect.PLANT, 5).add(Aspect.LIFE, 5));
         ThaumcraftApi.registerObjectTag(BlockTags.WOOL, new AspectList().add(Aspect.BEAST, 15).add(Aspect.CRAFT, 5));
         ThaumcraftApi.registerObjectTag(new ItemStack(Blocks.FIRE), new AspectList().add(Aspect.FIRE, 20));
         ThaumcraftApi.registerObjectTag(new ItemStack(Blocks.MAGMA_BLOCK), new AspectList().add(Aspect.FIRE, 10).add(Aspect.EARTH, 5));
 //        ThaumcraftApi.registerComplexObjectTag(new ItemStack(Blocks.TORCH, 1), new AspectList().add(Aspect.LIGHT, 5));
         ThaumcraftApi.registerObjectTag(new ItemStack(Items.FLINT), new AspectList().add(Aspect.EARTH, 5).add(Aspect.TOOL, 5));
+        ThaumcraftApi.registerObjectTag(new ItemStack(Items.STRING), new AspectList().add(Aspect.BEAST, 5).add(Aspect.CRAFT, 1));
         ThaumcraftApi.registerObjectTag(new ItemStack(Items.SLIME_BALL), new AspectList().add(Aspect.WATER, 5).add(Aspect.LIFE, 5).add(Aspect.ALCHEMY, 1));
         ThaumcraftApi.registerObjectTag(new ItemStack(Items.GUNPOWDER), new AspectList().add(Aspect.FIRE, 10).add(Aspect.ENTROPY, 10).add(Aspect.ALCHEMY, 5));
         ThaumcraftApi.registerObjectTag(new ItemStack(Items.INK_SAC), new AspectList().add(Aspect.WATER, 2).add(Aspect.BEAST, 2).add(Aspect.SENSES, 5));
